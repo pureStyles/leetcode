8c3c90b0059dccdf938c0e976089b160c2e9c340
pureStyles <siyuan.wen@outlook.com>
2024-02-29 00:56:36 +0800

    (合并两个有序数组): O(m+n)事件复杂度解决方式

--- src/Main.java
+++ src/Main.java
@@ -23,6 +23,30 @@ class Solution {
     }
     public static void main(String[] args){
        Solution solution = new Solution();
-       solution.merge(new int[]{1, 2, 3, 0, 0, 0}, 3, new int[]{2, 5, 6}, 3);
+       solution.mergeOptimize(new int[]{1, 2, 3, 0, 0, 0}, 3, new int[]{2, 5, 6}, 3);
+    }
+
+    /** 寻找更优的解法 */
+    public void mergeOptimize(int[] nums1, int m, int[] nums2, int n){
+        int p1 = m - 1, p2 = n-1;
+        while(p1 >= 0 && p2 >= 0){
+            if(nums2[p2] >= nums1[p1]){
+                // 把大的排在最后面
+                nums1[p1 + p2 + 1] = nums2[p2];
+                p2--;
+            } else {
+                nums1[p1 + p2 + 1] = nums1[p1];
+                p1--;
+            }
+        }
+        if(p2 >= 0){
+            // nums1的长度小于nums2的长度,这个时候nums1已经排完，直接把剩余的nums2移过来即可
+            for(int i = 0; i <= p2; i++){
+                nums1[i] = nums2[i];
+            }
+        }
+        for(int i = 0; i < m + n; i++){
+            System.out.println(nums1[i]);
+        }
     }
 }
