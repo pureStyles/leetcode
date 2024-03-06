7481d5985e7c359cf2461c7816abef3bb085c4d3
pureStyles <siyuan.wen@outlook.com>
2024-03-07 01:09:47 +0800

    (移除元素): 双指针算法优化

--- src/Solution_20240305.java
+++ src/Solution_20240305.java
@@ -11,11 +11,24 @@ public class Solution_20240305 {
         }
         return left;
     }
+    public int removeElementOptimize(int[] nums, int val){
+        int right = nums.length, left = 0;
+        while(left < right){
+            if(nums[left] != val){
+                left ++;
+            } else {
+                nums[left] = nums[right - 1];
+                right --;
+            }
+        }
+        return left;
+    }
 
     public static void main(String[] args){
         int[] nums = { 0, 1, 2, 2, 3, 0, 4 };
         Solution_20240305 solution20240305 = new Solution_20240305();
-        int len = solution20240305.removeElement(nums, 2);
+//        int len = solution20240305.removeElement(nums, 2);
+        int len = solution20240305.removeElementOptimize(nums, 2);
         for(int i = 0; i < len; i++){
             System.out.println(nums[i]);
         }
