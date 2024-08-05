05948898451f93fa6e7b0ea4e3fd023f3d2f0755
pureStyles <siyuan.wen@outlook.com>
2024-08-05 23:24:55 +0800

    (接雨水-超时): 通过按照列优先方法解决接雨水问题通过321/322

--- src/Solution_0805.java
+++ src/Solution_0805.java
@@ -1,5 +1,5 @@
 public class Solution_0805 {
-    public int trap(int[] height) {
+    public int trap1(int[] height) {
         int sum = 0;
         int len = height.length;
         int max = max(height);
@@ -29,5 +29,38 @@ public class Solution_0805 {
             }
         }
         return max;
+    }
+
+    public int trap(int[] height) {
+        int sum = 0;
+        int len = height.length;
+        for (int i = 1; i < len - 1; i++) {
+            /**
+             * 找出左侧最高的柱子
+             */
+            int maxLeft = 0;
+            for (int j = 0; j < i; j++) {
+                if (height[j] > maxLeft) {
+                    maxLeft = height[j];
+                }
+            }
+            /**
+             * 找出右侧最高的柱子
+             */
+            int maxRight = 0;
+            for (int j = i + 1; j < len; j++) {
+                if (height[j] > maxRight) {
+                    maxRight = height[j];
+                }
+            }
+            /**
+             * 找出两侧较低的那一个柱子
+             */
+            int less = Math.min(maxLeft, maxRight);
+            if (height[i] < less) {
+                sum += (less - height[i]);
+            }
+        }
+        return sum;
     }
 }
