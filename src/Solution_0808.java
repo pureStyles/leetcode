ccc9b630420747ba49aa5e978c262071deab4e64
pureStyles <siyuan.wen@outlook.com>
2024-08-08 23:43:43 +0800

    (longestCommonPrefix): 纵向遍历解决最长公共前缀

--- src/Solution_0808.java
+++ src/Solution_0808.java
@@ -1,5 +1,5 @@
 public class Solution_0808 {
-    public String longestCommonPrefix(String[] strs) {
+    public String longestCommonPrefix1(String[] strs) {
         if (strs == null || strs.length == 0) {
             return "";
         }
@@ -20,10 +20,25 @@ public class Solution_0808 {
         return str1.substring(0, index);
     }
 
+    public String longestCommonPrefix(String[] strs) {
+        // 随便选取一个长度
+        int len = strs[0].length();
+        int count = strs.length;
+        String c = strs[0];
+        for (int i = 0; i < len; i++) {
+            for (int j = 1; j <= count; j++) {
+                if (i == strs[j].length() || strs[j].charAt(i) != c.charAt(i)) {
+                    return c.substring(0, i);
+                }
+            }
+        }
+        return c;
+    }
+
     public static void main(String[] args) {
         Solution_0808 solution = new Solution_0808();
         String[] strs = {"flower", "flow", "flight"};
-        String prefix = solution.longestCommonPrefix(strs);
+        String prefix = solution.longestCommonPrefix1(strs);
         System.out.println(prefix);
     }
 }
