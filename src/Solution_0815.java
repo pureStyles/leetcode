4efe2bbaba147a8c591d200ea3093db557f0a196
pureStyles <siyuan.wen@outlook.com>
2024-08-18 00:35:45 +0800

    (满足条件的最小子数组)：滑动窗口思路解决最小子数组问题

--- src/Solution_0815.java
+++ src/Solution_0815.java
@@ -1,5 +1,5 @@
 public class Solution_0815 {
-    public int minSubArrayLen(int target, int[] nums) {
+    public int minSubArrayLen1(int target, int[] nums) {
         int res = Integer.MAX_VALUE;
         int len = nums.length;
         for (int i = 0; i < len; i++) {
@@ -12,6 +12,43 @@ public class Solution_0815 {
             }
         }
         return res == Integer.MAX_VALUE ? 0 : res;
+    }
+
+    public int minSubArrayLen2(int target, int[] nums) {
+        // 使用滑动窗口解决
+        int res = Integer.MAX_VALUE;
+        int len = nums.length;
+        int start = 0, end = 0, sum = 0;
+        sum += nums[start];
+        while (start <= end && start < len) {
+            while (sum < target) {
+                end++;
+                sum += nums[end >= len ? len - 1 : end];
+            }
+            if (end - start + 1 < res) {
+                res = end - start + 1;
+            }
+            sum = sum - nums[start];
+            start++;
+
+        }
+        return res == Integer.MAX_VALUE ? 0 : res;
+    }
+
+    public int minSubArrayLen(int target, int[] nums) {
+        int len = nums.length;
+        int start = 0, end = 0, sum = 0;
+        int result = Integer.MAX_VALUE;
+        while (end < len) {
+            sum += nums[end];
+            while (sum >= target) {
+                result = Math.min(result, end - start + 1);
+                sum = sum - nums[start];
+                start++;
+            }
+            end++;
+        }
+        return result == Integer.MAX_VALUE ? 0 : result;
     }
 
     public static void main(String[] args) {
