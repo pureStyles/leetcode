7c22f02b86a20dc53128b4e9a319f13f6fadb04e
pureStyles <siyuan.wen@outlook.com>
2024-08-22 23:32:24 +0800

    (两数之和): 引进头指针

--- src/Link/Solution_0822.java
+++ src/Link/Solution_0822.java
@@ -1,7 +1,7 @@
 package Link;
 
 public class Solution_0822 {
-    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+    public ListNode addTwoNumbers1(ListNode l1, ListNode l2) {
         ListNode result = new ListNode(0);
         while (l1 != null || l2 != null) {
             int sum = 0;
@@ -36,5 +36,29 @@ public class Solution_0822 {
         }
         ListNode newNode = new ListNode(value);
         head.next = newNode;
+    }
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode head = new ListNode(100);
+        ListNode cur = head;
+        int carry = 0;
+        while (l1 != null || l2 != null) {
+            int left = l1 == null ? 0 : l1.val;
+            int right = l2 == null ? 0 : l2.val;
+            int sum = carry + left + right;
+            carry = sum / 10;
+            cur.next = new ListNode(sum % 10);
+            cur = cur.next;
+            if (l1 != null) {
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                l2 = l2.next;
+            }
+            if (carry == 1) {
+                cur.next = new ListNode(1);
+            }
+        }
+        return head.next;
     }
 }
