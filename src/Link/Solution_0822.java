def753ced6a7563eb0d5a26bc42a00d4482aa40f
pureStyles <siyuan.wen@outlook.com>
2024-08-25 12:25:33 +0800

    (有序链表合并): 使用递归解法解决合并有序链表问题

--- src/Link/Solution_0822.java
+++ src/Link/Solution_0822.java
@@ -62,7 +62,7 @@ public class Solution_0822 {
         return head.next;
     }
 
-    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+    public ListNode mergeTwoLists1(ListNode list1, ListNode list2) {
         ListNode head = new ListNode(100);
         ListNode cur = head;
         while (list1 != null || list2 != null) {
@@ -84,5 +84,23 @@ public class Solution_0822 {
             }
         }
         return head.next;
+    }
+
+    // 递归解法
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        // 递归终止条件
+        if (l1 == null) {
+            return l2;
+        }
+        if (l2 == null) {
+            return l1;
+        }
+        if (l1.val < l2.val) {
+            l1.next = mergeTwoLists(l2, l1.next);
+            return l1;
+        } else {
+            l2.next = mergeTwoLists(l1, l2.next);
+            return l2;
+        }
     }
 }
