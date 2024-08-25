3e15bf627296f5725b4784339f384f4dc71600a0
pureStyles <siyuan.wen@outlook.com>
2024-08-25 22:17:02 +0800

    (反转链表): 通过不断地考虑各种情况解决反转链表

--- src/Link/Solution_0825.java
+++ src/Link/Solution_0825.java
@@ -19,4 +19,40 @@ public class Solution_0825 {
         }
         return map.get(head);
     }
+
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        if (head.next == null) {
+            return head;
+        }
+        if (left == right) {
+            return head;
+        }
+        ListNode cur = head;
+        int index = 1;
+        Map<Integer, ListNode> map = new HashMap<>();
+        map.put(0, new ListNode(1000));
+        while (cur != null) {
+            map.put(index, new ListNode(cur.val));
+            index++;
+            cur = cur.next;
+        }
+        map.get(left - 1).next = map.get(right);
+        if (index > right + 1) {
+            map.get(left).next = map.get(right + 1);
+        }
+        if (left > 1) {
+            map.get(0).next = map.get(1);
+        }
+        for (int i = 1; i < left - 1; i++) {
+            map.get(i).next = map.get(i + 1);
+        }
+        for (int i = right; i > left; i--) {
+            map.get(i).next = map.get(i - 1);
+        }
+        for (int i = right + 1; i < index - 1; i++) {
+            map.get(i).next = map.get(i + 1);
+        }
+
+        return map.get(0).next;
+    }
 }
