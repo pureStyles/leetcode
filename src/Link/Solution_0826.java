9700e8a346ba1f9a18fbcac53d345e03f983c451
pureStyles <siyuan.wen@outlook.com>
2024-08-26 23:45:14 +0800

    (删除链表的倒数第N个节点): 结合场景与stack的特性相符合，借助stack来解决

--- src/Link/Solution_0826.java
+++ src/Link/Solution_0826.java
@@ -1,7 +1,10 @@
 package Link;
 
+import java.util.List;
+import java.util.Stack;
+
 public class Solution_0826 {
-    public ListNode removeNthFromEnd(ListNode head, int n) {
+    public ListNode removeNthFromEnd1(ListNode head, int n) {
         ListNode vNode = new ListNode(100);
         vNode.next = head;
         ListNode pre = vNode, next, cur;
@@ -18,6 +21,23 @@ public class Solution_0826 {
         }
         // 此时的cur指着当前需要删除的节点
         pre.next = cur.next;
+        return vNode.next;
+    }
+
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode vNode = new ListNode(1000);
+        vNode.next = head;
+        ListNode cur = vNode;
+        Stack<ListNode> stack = new Stack<>();
+        while (cur != null) {
+            stack.push(cur);
+            cur = cur.next;
+        }
+        for (int i = 1; i <= n; i++) {
+            stack.pop();
+        }
+        ListNode pre = stack.peek();
+        pre.next = pre.next.next;
         return vNode.next;
     }
 }
