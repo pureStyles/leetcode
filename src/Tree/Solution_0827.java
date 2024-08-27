14904c43c8e66e5d4c911c3bfb2e0176c82e5008
pureStyles <siyuan.wen@outlook.com>
2024-08-28 00:32:15 +0800

    (二叉树最大深度): DFS解决二叉树的最大深度

--- src/Tree/Solution_0827.java
+++ src/Tree/Solution_0827.java
@@ -1,4 +1,20 @@
 package Tree;
 
 public class Solution_0827 {
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int maxDepth = 0;
+        if (root.left == null) {
+            return maxDepth(root.right) + 1;
+        }
+        if (root.right == null) {
+            return maxDepth(root.left) + 1;
+        }
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+        maxDepth = Math.max(left, right) + 1;
+        return maxDepth;
+    }
 }
