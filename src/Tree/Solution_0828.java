829135094ac827e8ec06a574114e90f933be52c5
pureStyles <siyuan.wen@outlook.com>
2024-08-28 23:51:23 +0800

    (对称二叉树): DFS判断二叉树是否是对称二叉树

--- src/Tree/Solution_0828.java
+++ src/Tree/Solution_0828.java
@@ -1,5 +1,7 @@
 package Tree;
 
+import com.sun.source.tree.Tree;
+
 public class Solution_0828 {
     public boolean isSameTree(TreeNode p, TreeNode q) {
         if (p == null && q == null) {
@@ -21,5 +23,19 @@ public class Solution_0828 {
         root.left = invertTree(root.right);
         root.right = invertTree(tmp);
         return root;
+    }
+
+    public boolean isSymmetric(TreeNode root) {
+        return root == null || isSymmetricTree(root.left, root.right);
+    }
+
+    public boolean isSymmetricTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+        if (p == null || q == null || p.val != q.val) {
+            return false;
+        }
+        return isSymmetricTree(p.left, q.right) && isSymmetricTree(p.right, q.left);
     }
 }
