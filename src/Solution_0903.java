d9b6245e25d9a717b9832fa5d85d792458e99f89
pureStyles <siyuan.wen@outlook.com>
2024-09-03 23:10:23 +0800

    (搜索二维矩阵): 使用2次二分查找搜索二维矩阵

--- src/Solution_0903.java
+++ src/Solution_0903.java
@@ -14,4 +14,51 @@ public class Solution_0903 {
         }
         return left;
     }
+
+    public boolean searchMatrix(int[][] matrix, int target) {
+        /**
+         * 第一次二分查找先找到在哪一行
+         */
+        int rowIndex = binarySearchFirstColumn(matrix, target);
+        if (rowIndex < 0) {
+            return false;
+        }
+        /**
+         * 第二次二分查找对此行进行二分查找
+         */
+        return binarySearchRow(matrix[rowIndex], target);
+    }
+
+    public int binarySearchFirstColumn(int[][] matrix, int target) {
+        int left = 0, right = matrix.length - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (matrix[mid][0] == target) {
+                return mid;
+            } else if (matrix[mid][0] < target) {
+                left = mid + 1;
+            } else {
+                //matrix[mid][0] > target
+                right = mid - 1;
+            }
+        }
+        // left是插入的位置，所在行索引应该-1
+        return left - 1;
+    }
+
+    public boolean binarySearchRow(int[] row, int target) {
+        int left = 0, right = row.length - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (row[mid] == target) {
+                return true;
+            } else if (row[mid] < target) {
+                left = mid + 1;
+            } else {
+                // row[mid] > target
+                right = mid - 1;
+            }
+        }
+        return false;
+    }
 }
