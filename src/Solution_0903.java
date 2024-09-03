854d2394f10bd54c418109cb90b74741fe27ee85
pureStyles <siyuan.wen@outlook.com>
2024-09-03 22:36:46 +0800

    (搜索插入位置): 通过二分查找搜索插入的位置

---
+++ src/Solution_0903.java
@@ -0,0 +1,17 @@
+public class Solution_0903 {
+    public int searchInsert(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                // nums[mid] > target
+                right = mid - 1;
+            }
+        }
+        return left;
+    }
+}
