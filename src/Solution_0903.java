a0ade53ebae1e8c42706839a68d9a01a7cc86a82
pureStyles <siyuan.wen@outlook.com>
2024-09-03 23:50:20 +0800

    (寻找峰值): 使用2分查找解决峰值查找

--- src/Solution_0903.java
+++ src/Solution_0903.java
@@ -61,4 +61,17 @@ public class Solution_0903 {
         }
         return false;
     }
+
+    public int findPeakElement(int[] nums) {
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] < nums[mid + 1]) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return left;
+    }
 }
