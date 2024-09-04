47391d1498ab31001b44db2d3d021ba15afc5871
pureStyles <siyuan.wen@outlook.com>
2024-09-04 22:46:50 +0800

    (爬楼梯): 一维动态规划解决问题

---
+++ src/DynamicPlan/Solution_0904.java
@@ -0,0 +1,14 @@
+package DynamicPlan;
+
+public class Solution_0904 {
+    public int climbStairs(int n) {
+        int[] dp = new int[50];
+        dp[0] = 1;
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+}
