97c881b750f4c68649fb6ae73914a9df223df9af
pureStyles <siyuan.wen@outlook.com>
2024-09-06 00:23:51 +0800

    (单词拆分): 一维动态规划解决单词拆分问题

---
+++ src/DynamicPlan/Solution_0906.java
@@ -0,0 +1,21 @@
+package DynamicPlan;
+
+import java.util.HashSet;
+import java.util.List;
+
+public class Solution_0906 {
+    public boolean wordbreak(String s, List<String> wordDict) {
+        HashSet<String> set = new HashSet<>(wordDict);
+        boolean[] dp = new boolean[s.length() + 1];
+        dp[0] = true;
+        for (int i = 1, len = s.length(); i <= len; i++) {
+            for (int j = 0; j < i; j++) {
+                if (dp[j] && set.contains(s.substring(j, i))) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[s.length()];
+    }
+}
