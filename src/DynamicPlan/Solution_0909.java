2e04279a565f5658e8ff22dda71432c9ab7d9321
pureStyles <siyuan.wen@outlook.com>
2024-09-09 23:54:36 +0800

    (最长递增子序列): 动态规划

---
+++ src/DynamicPlan/Solution_0909.java
@@ -0,0 +1,23 @@
+package DynamicPlan;
+
+public class Solution_0909 {
+    public int lengthOfLIS(int[] nums) {
+        int len = nums.length;
+        if (len == 0) {
+            return 0;
+        }
+        int[] dp = new int[len];
+        dp[0] = 1;
+        int max = 1;
+        for (int i = 1; i < len; i++) {
+            dp[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            max = Math.max(max, dp[i]);
+        }
+        return max;
+    }
+}
