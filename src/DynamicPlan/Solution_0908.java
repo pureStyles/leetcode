d674bfa82a550af22cb1d493c865fc9cfd6d9647
pureStyles <siyuan.wen@outlook.com>
2024-09-09 23:27:49 +0800

    (零钱兑换): 动态规划

--- src/DynamicPlan/Solution_0908.java
+++ src/DynamicPlan/Solution_0908.java
@@ -1,9 +1,11 @@
 package DynamicPlan;
 
+import java.util.Arrays;
+
 public class Solution_0908 {
     int res = Integer.MAX_VALUE;
 
-    public int coinChange(int[] coins, int amount) {
+    public int coinChange1(int[] coins, int amount) {
         int len = coins.length;
         if (len == 0) {
             return -1;
@@ -26,4 +28,19 @@ public class Solution_0908 {
             findWay(coins, amount - coins[i], count++);
         }
     }
+
+    public int coinChange(int[] coins, int amount) {
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, amount + 1);
+        dp[0] = 0;
+        for (int i = 1; i <= amount; i++) {
+            for (int j = 0, len = coins.length; j < len; j++) {
+                if (coins[j] <= i) {
+                    dp[i] = Math.max(dp[i], dp[i - coins[j]] + 1);
+                }
+            }
+        }
+        return dp[amount] > amount ? -1 : dp[amount];
+    }
+
 }
